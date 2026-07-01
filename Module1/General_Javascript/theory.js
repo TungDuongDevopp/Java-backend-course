ad26e0d14d538dba46bc69a0866b7183fd8dbc05
TungDuong-Dev <devopptungduong@gmail.com>
2026-07-01 20:01:27 +0700

    BTVN Tổng quan về javascript

---
+++ Module1/General_Javascript/theory.js
@@ -0,0 +1,8 @@
+let ten;
+let tuoi;
+
+ten = prompt("Mời bạn nhập tên:")
+tuoi = parseInt(prompt("Mời bạn nhập tuổi"));
+
+document.writeln("Tên của bạn là: ",ten,"<br>");
+document.writeln("Tuổi của bạn là: ",tuoi)
