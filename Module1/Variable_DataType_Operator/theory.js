668f9c4a19bb374a61e91bb72ff5cbe974ddac01
TungDuong-Dev <devopptungduong@gmail.com>
2026-07-03 21:33:34 +0700

    Lý thuyết bài toán tử, biến,kiểu dữ liệu

--- Module1/Variable_DataType_Operator/theory.js
+++ Module1/Variable_DataType_Operator/theory.js
@@ -1,2 +1,22 @@
+// Kiểu dữ liệu
+//Kiểu số
+let number = 12;
 
+console.log(typeof number);
+
+//Kiểu chuỗi
+let name ="Dương đẹp trai"
+let nameOfClass = 'C06';
+let nameOfSchool = `HUMG`;
+
+//Kiểu logic (boolean)
+let isSick = true;
+let isHappy = false;
+console.log(typeof isSick)
+
+// Toán tử +,-,*,/, %
+
+//Toán tử gán =,+=,-=,*=,/=
+
+//Khi chuỗi thực hiện ngoài dấu + đều chuyển thành số
 
